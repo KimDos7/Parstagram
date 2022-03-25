a8b34ebb47817894b45c52cbeb4a83fbcfe51a55
Alex Kim <youngsuk.dory@gmail.com>
2022-03-25 18:42:23 -0400

    Initial Code Commits

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import Parse
 
-@main
+ @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "NT4xYhbGnJM0GsJpOVPZrXjQ2BnCEFp52QuUn8Qq" // <- UPDATE
+                $0.clientKey = "gJmmowyVmispsIKWPKUNfh4FerWKZpaeTLaziDo2" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
